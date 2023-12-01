286586f69636e214709ed3c8aca9b31536db223e
Dylan Figueredo <d.figueredo2104@gmail.com>
2023-12-01 17:00:35 -0600

    First Commit

---
+++ src/commands/misc/github.js
@@ -0,0 +1,15 @@
+const { Client, Interaction, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    /**
+     * 
+     * @param {Client} client 
+     * @param {Interaction} interaction 
+     */
+    callback: async (client, interaction) => {
+        
+    },
+
+    name: 'github',
+    description: '',
+};
---
+++ src/commands/moderation/purge.js
@@ -0,0 +1,15 @@
+const { Client, Interaction } = require('discord.js');
+
+module.exports = {
+    /**
+     * 
+     * @param {Client} client 
+     * @param {Interaction} interaction 
+     */
+    callback: async (client, interaction) => {
+        
+    },
+
+    name: 'purge',
+    description: '',
+};
